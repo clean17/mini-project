7a8c0d2789eaec949b77ed907d1bd9f88021f5a1
YouHyunKim <yuhyunk7@gmail.com>
2023-02-23 10:22:20 +0900

    테스트

--- src/main/java/shop/mtcoding/project/exception/CustomException.java
+++ src/main/java/shop/mtcoding/project/exception/CustomException.java
@@ -12,6 +12,7 @@ public class CustomException extends RuntimeException {
     public CustomException(String message) {
         this(message, HttpStatus.BAD_REQUEST);
     }
+
     public CustomException(String message, HttpStatus status) {
         super(message);
         this.status = status;
